c73fd461877e1159909732a204e1cd3907124fee
Matthew Liew <mlie@camelot-itlab.com>
2023-04-13 14:13:19 +0200

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
+int main() {
+    //image
+    const int image_width = 256;
+    const int image_height = 256;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    //render
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j){
+        for (int i=0; i<image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
     }
-    cout << endl;
 }
